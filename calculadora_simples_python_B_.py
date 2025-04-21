187ef4087d1b9518d18457098e6368d572bb93ad
Amanda Lino <amanda.lino.vds@gmail.com>
2025-04-20 23:07:23 -0300

    ini

--- calculadora_simples_python_B_.py
+++ calculadora_simples_python_B_.py
@@ -1,7 +1,7 @@
 num1=float(input("digite o primeiro numero: "))
 num2=float(input("digite o segundo numero: "))
 
-operacao=input("escolha a operação (+,-,*,/): ")
+operacao=input("escolha a operacao (+,-,*,/): ")
 if operacao=='+':
     resultado=num1+num2
 elif operacao=='-':
@@ -12,8 +12,8 @@ elif operacao=='/':
     if num2 != 0:
         resultado=num1/num2
     else: 
-        resultado="erro: divisão por zero não permitida"
+        resultado="erro: divisao por zero nao permitida"
 else: 
-    resultado="operação invalida. Escolha entre +,-,* ou /"
+    resultado="operacao invalida. Escolha entre +,-,* ou /"
 
 print ("resultado: ", resultado)
